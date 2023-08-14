c34f3632595cae12b45f6abfaee6bed053474ff3
abdaziz-04 <abdaziz1713@gmail.com>
2023-08-14 21:44:25 +0700

    all

---
+++ app/src/main/java/com/lsp/sistemakademik/ListMahasiswaActivity.java
@@ -0,0 +1,14 @@
+package com.lsp.sistemakademik;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListMahasiswaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_mahasiswa);
+    }
+}
