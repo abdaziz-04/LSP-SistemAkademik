070931ad599ca328cf0d8d97a1e22e558343f059
abdaziz-04 <abdaziz1713@gmail.com>
2023-08-15 10:20:00 +0700

    DB Helper

--- app/src/main/java/com/lsp/sistemakademik/helper/Database.java
+++
@@ -1,4 +0,0 @@
-package com.lsp.sistemakademik.helper;
-
-public class Database {
-}
---
+++ app/src/main/java/com/lsp/sistemakademik/helper/Helper.java
@@ -0,0 +1,69 @@
+package com.lsp.sistemakademik.helper;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Helper extends SQLiteOpenHelper {
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "kampus";
+
+    public Helper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        final String SQL_CREATE_TABLE = "CREATE TABLE mahasiswa (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, jenisKelamin TEXT, tglLahir TEXT, alamat TEXT)";
+        sqLiteDatabase.execSQL(SQL_CREATE_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS mahasiswa");
+        onCreate(sqLiteDatabase);
+    }
+
+    public ArrayList<HashMap<String, String>> getAll() {
+        ArrayList<HashMap<String, String>> list = new ArrayList<>();
+        String QUERY = "SELECT * FROM mahasiswa";
+        SQLiteDatabase database = this.getWritableDatabase();
+        Cursor cursor = database.rawQuery(QUERY, null);
+        if (cursor.moveToFirst()) {
+            do {
+                HashMap<String, String> map = new HashMap<>();
+                map.put("id", cursor.getString(0));
+                map.put("name", cursor.getString(1));
+                map.put("jenisKelamin", cursor.getString(2));
+                map.put("tglLahir", cursor.getString(3));
+                map.put("alamat", cursor.getString(4));
+
+                list.add(map);
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+        return list;
+    }
+
+    public void insert (String name, String jenisKelamin, String tglLahir, String alamat) {
+        SQLiteDatabase database = this.getWritableDatabase();
+        String QUERY = "INSERT INTO mahasiswa (name, email) VALUES ('"+name+"','"+jenisKelamin+"', '"+tglLahir+"', '"+alamat+"')";
+        database.execSQL(QUERY);
+    }
+
+    public void update (int id, String name, String jenisKelamin, String tglLahir, String alamat) {
+        SQLiteDatabase database = this.getWritableDatabase();
+        String QUERY = "UPDATE mahasiswa SET name = '"+name+"','"+jenisKelamin+"', '"+tglLahir+"', '"+alamat+"' WHERE id = " + id;
+        database.execSQL(QUERY);
+    }
+
+    public void DELETE (int id) {
+        SQLiteDatabase database = this.getWritableDatabase();
+        String QUERY = "DELETE FROM mahasiswa WHERE id = " + id;
+        database.execSQL(QUERY);
+    }
+}
