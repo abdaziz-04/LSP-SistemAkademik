77208d2abaea3ebb5b1c7ecbb7d73d49f7b524ab
abdaziz-04 <abdaziz1713@gmail.com>
2023-08-15 11:03:16 +0700

    Adapter

--- app/src/main/java/com/lsp/sistemakademik/adapter/Adapter.java
+++ app/src/main/java/com/lsp/sistemakademik/adapter/Adapter.java
@@ -1,4 +1,67 @@
 package com.lsp.sistemakademik.adapter;
 
-public class Adapter {
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.lsp.sistemakademik.R;
+import com.lsp.sistemakademik.model.Data;
+
+import org.w3c.dom.Text;
+
+import java.util.List;
+
+public class Adapter extends BaseAdapter {
+    private Activity activity;
+    private LayoutInflater inflater;
+    private List<Data> lists;
+
+    TextView Name, tglLahir, Alamat, jenisKelamin;
+
+    public Adapter(Activity activity, List<Data> lists) {
+        this.activity = activity;
+        this.lists = lists;
+    }
+
+    @Override
+    public int getCount() {
+        return lists.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return lists.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        if (inflater == null) {
+            inflater = (LayoutInflater) activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        }
+        if (view == null) {
+            view = inflater.inflate(R.layout.activity_list_mahasiswa, null);
+        }
+        Name = view.findViewById(R.id.tv_nama);
+        jenisKelamin = view.findViewById(R.id.tv_jenisKelamin);
+        tglLahir = view.findViewById(R.id.tv_tglLahir);
+        Alamat = view.findViewById(R.id.tv_alamat);
+
+        Data data = lists.get(i);
+
+        Name.setText(data.getName());
+        jenisKelamin.setText(data.getJenisKelamin());
+        tglLahir.setText(data.getTglLahir());
+        Alamat.setText(data.getAlamat());
+
+        return view;
+    }
 }
