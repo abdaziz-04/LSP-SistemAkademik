97d4e3f77752a532bb4a28031b3aa5f1d0ba7fe0
abdaziz-04 <abdaziz1713@gmail.com>
2023-08-16 16:57:12 +0700

    fixing bug

--- app/src/main/java/com/lsp/sistemakademik/DetailDataActivity.java
+++ app/src/main/java/com/lsp/sistemakademik/DetailDataActivity.java
@@ -2,17 +2,22 @@ package com.lsp.sistemakademik;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class DetailDataActivity extends AppCompatActivity {
     TextView nama, jenisKelamin, tglLahir, alamat;
+    Button btnOK;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_data);
 
+        btnOK = findViewById(R.id.bt_ok);
         // Inisialisasi TextView
         nama = findViewById(R.id.tv_nama);
         jenisKelamin = findViewById(R.id.tv_jenisKelamin);
@@ -33,5 +38,13 @@ public class DetailDataActivity extends AppCompatActivity {
             tglLahir.setText( tglLahirValue);
             alamat.setText( alamatValue);
         }
+
+btnOK.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent(DetailDataActivity.this, ListMahasiswaActivity.class);
+        startActivity(intent);
+    }
+});
     }
 }
